af740c6cba7e8102c21acac06ca200b77cec0c24
Helix088 <pat20001@byui.edu>
2022-04-28 09:06:01 -0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
+// ignore_for_file: missing_required_param
+
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int note) {
+    AudioCache player = AudioCache();
+    player.play('note$note.wav');
+  }
+
+  Expanded buildButton({MaterialColor color, int note}) {
+    return Expanded(
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+        ),
+        onPressed: () {
+          playSound(note);
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildButton(color: Colors.red, note: 1),
+              buildButton(color: Colors.orange, note: 2),
+              buildButton(color: Colors.yellow, note: 3),
+              buildButton(color: Colors.green, note: 4),
+              buildButton(color: Colors.teal, note: 5),
+              buildButton(color: Colors.blue, note: 6),
+              buildButton(color: Colors.purple, note: 7),
+            ],
+          ),
         ),
       ),
     );
